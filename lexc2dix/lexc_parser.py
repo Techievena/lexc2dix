d9dbc8535b2a3a480bd6ebb8d88298903c441d7b
Techievena <abinashsena@gmail.com>
2018-03-02 21:20:49 +0530

    lexc_parser: Make the parsing module more organised.
    Put all the parsing module into single directory and
    use a file for parsing lexc parsing.

---
+++ lexc2dix/lexc_parser.py
@@ -0,0 +1,6 @@
+from parser import lex, yacc
+
+class lexc_parser:
+    def multichar_symbols_parser(self):
+        """The module to parse Multichar_symbols section"""
+        print("Multichar_symbols")
