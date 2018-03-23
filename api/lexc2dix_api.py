d8e7ecde912090be11fdde9270ddaab224e1789f
Techievena <abinashsena@gmail.com>
2018-03-23 08:02:30 +0530

    api: Rename directory
    Rename the bin directory to api for better clarification.

---
+++ api/lexc2dix_api.py
@@ -0,0 +1,45 @@
+from sys import exit
+import argparse
+import lexc2dix.lexc_parser as lp
+
+def main():
+    """Parse the arguments using argparse package"""
+    argparser = argparse.ArgumentParser(description='lexc2dix')
+
+    argparser.add_argument("-e", action="store_true", default=False)
+    argparser.add_argument("-V", "--version", action="store_true",
+                           default=False)
+    argparser.add_argument("filename", metavar="filename", type=str,
+                           nargs='+', default=False)
+
+    args = argparser.parse_args()
+    process(args)
+
+def process(args):
+    """Process the arguments. Call engine if flags are used."""
+    if args.e:
+        detailed_usage()
+        exit(2)
+    if args.version:
+        from lexc2dix import release
+        print(release.__version__)
+        exit(2)
+    if args.filename:
+        file_read(args.filename)
+
+def detailed_usage():
+    """Detailed documentation of lexc2dix."""
+    print("Welcome to the detailed documentation of lexc2dix !")
+
+def file_read(filename):
+    """Read the dictionary file parsed as arguement"""
+    filename = str(filename)
+    nline = ""
+    with open('filename', 'r') as lines:
+        for line in lines:
+            nline += (line.rstrip('\n').strip(' ').strip('\t')+'\n') if len(line.rstrip('\n').strip(' ').strip('\t')) else ''
+    lines.close()
+    lp.dict_split(nline)
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 import sys
 import os
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 from setuptools import find_packages, setup
 from lexc2dix import release
 
@@ -25,23 +25,23 @@ with open("README.md") as readme:
 
 if __name__ == "__main__":
     setup(
-        name = release.name,
-        version = release.__version__,
-        author = release.__author__,
-        author_email = release.__email__,
-        description = release.__description__,
-        url = release.__url__,
-        download_url = release.__download_url__,
-        keywords= ['lexc', 'twolc', 'monodix'],
+        name=release.name,
+        version=release.__version__,
+        author=release.__author__,
+        author_email=release.__email__,
+        description=release.__description__,
+        url=release.__url__,
+        download_url=release.__download_url__,
+        keywords=['lexc', 'twolc', 'monodix'],
         packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
         install_requires=required,
         tests_require=test_required,
         long_description=long_description,
-        scripts = ['bin/lexc2dix_api'],
-        license = 'GNU GENERAL PUBLIC LICENSE',
-        entry_points = {
+        scripts=['api/lexc2dix_api'],
+        license='GNU GENERAL PUBLIC LICENSE',
+        entry_points={
             'console_scripts': [
-            'lexc2dix = lexc2dix_api:main'
+                'lexc2dix = lexc2dix_api:main'
             ]
         }
     )
